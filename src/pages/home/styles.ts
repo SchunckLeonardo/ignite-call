57d7e77e9f1e07c229581bb4ef8c6d4d4edbfca7
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2024-04-03 08:58:03 -0300

    Home page done

---
+++ src/pages/home/styles.ts
@@ -0,0 +1,28 @@
+import { styled, Heading, Text } from '@ignite-ui/react'
+
+export const Container = styled('div', {
+  maxWidth: 'calc(100vw - (100vw - 1160px) / 2)',
+  height: '100vh',
+  display: 'flex',
+  alignItems: 'center',
+  gap: '$20',
+  marginLeft: 'auto',
+  overflow: 'hidden',
+})
+
+export const Hero = styled('div', {
+  maxWidth: 480,
+  padding: '0 $10',
+
+  [`> ${Heading}`]: {},
+
+  [`> ${Text}`]: {
+    marginTop: '$2',
+    color: '$gray200',
+  },
+})
+
+export const Preview = styled('div', {
+  paddingRight: '$8',
+  overflow: 'hidden',
+})
